6549d2f958f50b4f82deb9ef7131d2b87a2d5f1c
Anis FETOUI <anis.fetoui@esprit.tn>
2024-02-23 02:28:34 +0100

    workshop 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ResidencesComponentComponent } from './residences-component/residences-component.component';
+import { ApartementsComponent } from './apartements/apartements.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:"residence",pathMatch:'full'},
+  {path:'residence',component:ResidencesComponentComponent},
+  {path:'apartements/:idR',component:ApartementsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ResidencesComponentComponent } from './residences-component/residences-component.component';
+import { ApartementsComponent } from './apartements/apartements.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResidencesComponentComponent,
+    ApartementsComponent
   ],
   imports: [
     BrowserModule,
